16c8b537fc3af0d1f0e16226bb038e9ae433b5b2
luizduca <luiz.ducas.rocha@escola.pr.gov.br>
2024-11-04 11:33:12 -0300

    Add files via upload

---
+++ mat 2/assets/main.js
@@ -0,0 +1,21 @@
+function criaCartao(tema,pergunta,resposta) {
+   const conteiner=  document.getElementById('conteiner');
+   let cartao =document.createElement('article');
+   cartao.className='cartao';
+   cartao.innerHTML=    `
+   <div class="cartao-conteudo">
+                    <h3> ${tema} </h3>
+                    <div class="cartao-conteudo-pergunta">
+                        ${pergunta}
+                    </div>
+                    <div class="cartao-conteudo-resposta">
+                        ${resposta}
+                    </div>
+                    </div>
+
+
+   `
+conteiner.appendChild(cartao)
+}
+
+criaCartao('históra','em que ano inicio-se a guerra do vietnã?','em 1955')
